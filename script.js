cc07f71850e70d3df6ab87866abf37bd9344131a
hydravizion <68737046+hydravizion@users.noreply.github.com>
2025-08-07 00:30:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ const axios = require('axios');
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 
 async function postExchangeRate() {
-  const res = await axios.get('https://api.exchangerate.host/latest?base=USD&symbols=EUR,MYR,JPY');
+  const res = await axios.get('https://api.exchangerate.host/latest?base=MYR&symbols=JPY');
   const rates = res.data.rates;
 
   const message = {
-    username: 'CurrencyBot',
+    content: "Harini punya jpy",
     embeds: [
       {
         title: '📊 Daily Currency Exchange Rates (Base: USD)',
         color: 0x3498db,
         fields: [
-          { name: '🇪🇺 EUR', value: rates.EUR.toFixed(2), inline: true },
-          { name: '🇲🇾 MYR', value: rates.MYR.toFixed(2), inline: true },
           { name: '🇯🇵 JPY', value: rates.JPY.toFixed(2), inline: true },
         ],
         footer: { text: 'Source: exchangerate.host' },
